f3554a5c53a17dd1eed64b50459d6438bc8c561d
shelltitan <17dszi@gmail.com>
2020-08-05 21:12:14 +0200

    Update DeepVOG_model.py

--- deepvog/model/DeepVOG_model.py
+++ deepvog/model/DeepVOG_model.py
@@ -33,7 +33,6 @@ def encoding_block(X, filter_size, filters_num, layer_num, block_type, stage, s 
 def cond1(X_in):
     return X_in
 def cond2(X_in, X_jump_in = 0):
-    X_joined_input = Add()([X_in,X_jump_in])
     X_joined_input = Concatenate(axis = 3)([X_in,X_jump_in])
     return X_joined_input
 
