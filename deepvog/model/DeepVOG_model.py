7ef2ea19a20349da4f0e5dadfd0102281bb50aa9
shelltitan <17dszi@gmail.com>
2020-08-05 21:02:11 +0200

    Update DeepVOG_model.py

--- deepvog/model/DeepVOG_model.py
+++ deepvog/model/DeepVOG_model.py
@@ -43,7 +43,7 @@ def decoding_block(X, filter_size, filters_num, layer_num, block_type, stage, s 
     conv_name_base = 'conv_' + block_type + str(stage) + '_'
     bn_name_base = 'bn_' + block_type + str(stage)  + '_'
     
-    X_joined_input = tf.cond(tf.equal(X_jump, 0), cond1(X), cond2(X,X_jump))
+    X_joined_input = tf.cond(tf.equal(X_jump, 0), cond1(X_in = X), cond2(X_in = X,X_jump))
 #     Joining X_jump from encoding side with X_uped
 #    if X_jump == 0:
 #        X_joined_input = X
